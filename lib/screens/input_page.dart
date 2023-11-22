a8d7e57f1e3ec2ecd12618f4b1c009ac7245cd2e
cryleafing <73317225+cryleafing@users.noreply.github.com>
2023-11-22 16:46:51 +0000

    sort the background statless layout

--- lib/screens/input_page.dart
+++ lib/screens/input_page.dart
@@ -11,8 +11,41 @@ class InputPage extends StatelessWidget {
       home: Scaffold(
         body: ListView(children: [
           //put children here of widgets and etc
+          GreyBackgroundBox(),
         ]),
       ),
     );
+  }
+}
+
+class GreyBackgroundBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Container(
+        width: 360,
+        height: 800,
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(color: const Color.fromARGB(255, 18, 32, 47)),
+        child: Stack(
+          children: [
+            Positioned(
+              left: 0,
+              top: 97,
+              child: Container(
+                width: 360,
+                height: 745,
+                decoration: ShapeDecoration(
+                  color: Color(0xFFD9D9D9),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      )
+    ]);
   }
 }
